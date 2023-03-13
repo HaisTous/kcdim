7ea7b3a3e1378c9fba6d24508cbf6ecd8e92d655
HaisTous <vlad102013@mail.ru>
2023-03-13 17:34:26 +0300

    Исправление ошибок.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/kcdim-vue/',
+  base: '/kcdim/',
   plugins: [vue()],
   resolve: {
     alias: {
